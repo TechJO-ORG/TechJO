ecd925c40b17d49b1ea30f569c3c6b15712be5fc
Abdalwahab Abukhalil <abdabukhalil22@gmail.com>
2025-07-08 22:09:14 +0300

    Fix about pages

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
+import Techstack from "./Techstack";
 
 function About() {
   return (
@@ -31,6 +32,13 @@ function About() {
             <img src={laptopImg} alt="about" className="img-fluid" />
           </Col>
         </Row>
+
+        <h1 className="project-heading">
+          Professional <strong className="purple">Skillset </strong>
+        </h1>
+
+        <Techstack />
+
       </Container>
     </Container>
   );
